64889a67406c8be4a372c76bc8e3b75dff64b40b
saritagangbhoj <saritagangbhoj@gmail.com>
2022-05-28 01:06:32 +0530

    rendering multiple components

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,6 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      {/* <h1 data-ns-test="project-name">Amazon</h1>
-      <p data-ns-test="project-description">Ecommerce</p> */}
-
       <h1 data-ns-test="project-name">Google</h1>
       <p data-ns-test="project-description">Google is a popular internet Search Engine. It scans the Web to find Web pages that are relevant to the words you have typed in the search box.</p>
     </div>
